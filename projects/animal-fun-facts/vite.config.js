b70a359907496ee491c38cdb21fb9336443a918f
Char <char.mcgarry@gmail.com>
2025-08-21 11:02:15 +0100

    Trying to get animal-fun-facts running on GitHub pages

--- projects/animal-fun-facts/vite.config.js
+++ projects/animal-fun-facts/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     base: '/projects/animal-fun-facts/',
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: "index.dev.html",
+        },
+    }
 });
