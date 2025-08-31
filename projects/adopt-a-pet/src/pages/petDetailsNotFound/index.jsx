9029b2a45bcf2853fb0cb2703621553c566616c6
Char <char.mcgarry@gmail.com>
2025-08-31 14:36:46 +0100

    Fixing style issue with paragraph on petDetailsNotFound page

--- projects/adopt-a-pet/src/pages/petDetailsNotFound/index.jsx
+++ projects/adopt-a-pet/src/pages/petDetailsNotFound/index.jsx
@@ -9,11 +9,15 @@ const PetDetailsNotFound = () => {
   const goHome = () => {
     navigate(`${projectFolder}/`);
   }
+
+  const centered = {
+    textAlign: 'center'
+  }
   
   return (
     <main className="page">
       <h3>404: Who let the dogs out?</h3>
-      <p style='text-align:center'>Sorry, but further details for this pet have not been uploaded by the shelter yet. Check back later!</p>
+      <p style={centered}>Sorry, but further details for this pet have not been uploaded by the shelter yet. Check back later!</p>
       <img
         src="https://i.chzbgr.com/full/8362031616/h9EB970C5/weve-lost-our-corgination"
         alt=""
